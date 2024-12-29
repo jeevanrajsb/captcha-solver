4cd8578b3eebf50e54caf5bf1da0550950dacd6d
Taha <102474598+TahaBakhtari@users.noreply.github.com>
2024-12-29 20:36:16 +0330

    Delete Captcha directory

--- Captcha/useapi.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://localhost:8000/solve-captcha/'
-with open('captchas/captcha_325.png', 'rb') as f:
-    files = {'file': f}
-    response = requests.post(url, files=files)
-print(response.json())
